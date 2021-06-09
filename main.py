16ee815869766d3b813ebd599e954169ac506063
mic <micmor.m@gmail.com>
2021-06-08 23:41:56 -0400

    Update range for random int

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ print(logo)
 print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a number between1 and 100.")
 
-number = random.randint(1, 101)
+number = random.randint(1, 100)
 difficulty = input("Choose a difficulty. type 'easy' or 'hard': ")
 
 attempts = 10
